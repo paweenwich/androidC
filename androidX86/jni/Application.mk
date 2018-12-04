13f169b5ee35398fe362f3f4c97c0f829e8caed2
paweenwich <paweenwich@yahoo.com>
2018-12-04 14:58:29 +0700

    luac

--- androidX86/jni/Application.mk
+++ androidX86/jni/Application.mk
@@ -1,6 +1,8 @@
 #APP_PLATFORM := android-19
 APP_ABI := x86
 APP_STL := stlport_static
+#APP_MODULES := lualib
+	
 #APP_PIE := true
 
 #APP_CXXFLAGS := -Wall -Werror -fpermissive -fpic
--- androidX86/jni/luac/Android.mk
+++ androidX86/jni/luac/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE 	:= liblua
+LOCAL_MODULE 	:= liblualib
 LOCAL_SRC_FILES := lapi.c lauxlib.c lbaselib.c lbitlib.c lcode.c lcorolib.c lctype.c ldblib.c ldebug.c \
 	    ldo.c ldump.c lfunc.c lgc.c linit.c liolib.c lmathlib.c lmem.c loadlib.c lobject.c lopcodes.c\
 	    loslib.c lparser.c lstate.c lstring.c lstrlib.c ltable.c ltablib.c ltm.c lundump.c\
@@ -17,7 +17,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := luac
 LOCAL_SRC_FILES := luac.c
 #LOCAL_CPPFLAGS := -std=c++11         # whatever g++ flags you like
-LOCAL_STATIC_LIBRARIES := liblua
+LOCAL_STATIC_LIBRARIES := liblualib
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog   # whatever ld flags you like
 LOCAL_CFLAGS += -Wno-write-strings -Wformat -save-temps -g# -O0
 
@@ -25,10 +25,10 @@ include $(BUILD_EXECUTABLE)    # <-- Use this to build an executable.
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := luaexec
+LOCAL_MODULE := lua
 LOCAL_SRC_FILES := lua.c
 #LOCAL_CPPFLAGS := -std=c++11         # whatever g++ flags you like
-LOCAL_STATIC_LIBRARIES := liblua
+LOCAL_STATIC_LIBRARIES := liblualib
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog   # whatever ld flags you like
 LOCAL_CFLAGS += -Wno-write-strings -Wformat -save-temps -g# -O0
 
